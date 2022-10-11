1649f1584639d8c9835349bfa6f593fd6de55718
Nguyen Minh Quang <1653071@student.hcmus.edu.vn>
2022-10-11 07:38:12 +0700

    first commit

--- routes/productRouter.ts
+++ routes/productRouter.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { uploadFiles1 } from '../ultis/upload';
-import { middleware, validate } from '../middlewares/check';
+import { middleware} from '../middlewares/check';
 import { MimeTypes } from '../config/image';
 import { uploadFiles, upload1 } from '../ultis/upload';
+import validate from '../middlewares/validate.middleware';
+var {validationResult} = require('express-validator');
 const 	productRouter = express.Router();
 /**
  * @openapi
@@ -15,9 +17,29 @@ const 	productRouter = express.Router();
  *       200:
  *         description: API is  running
  */
-productRouter.get('/seed/:playgroundId',[middleware.checkPlaygroundId], (req,res) => {
-	MimeTypes.indexOf(req.file.MimeTypes)
-	return res.status(200).send("Playground id not existsdasdass")
+productRouter.post('/seed',validate.validateRegisterUser, (req,res) => {
+	const errors = validationResult(req);
+	/// 0 new
+	/// 1 inp
+	/// 2 done
+	/// 3 rejected
+	/// 4 renew
+     const ticketStatus = 0;
+	const changeStatus = 1;
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+    return;
+  }
+
+  if(ticketStatus === 0 && !(changeStatus ===1 ||changeStatus ===3 )){}
+
+  else if (ticketStatus === 1 && !(changeStatus === 2||changeStatus ===3 )) 
+
+  else if (ticketStatus === 2 && !(changeStatus === 4) 
+
+  else if (ticketStatus === 3 && !(changeStatus === 4) 
+  else if (ticketStatus === 4 && !(changeStatus === 2||changeStatus ===3)) 
+
 })
 
 productRouter.post('/uploadfile', upload1.single('myFile'), (req: any, res, next) => {
---
+++ services/user.service.ts
@@ -0,0 +1,11 @@
+const UserService = {
+	userInPlayground: async (data:{}) => {
+
+	},
+
+	userIsDeleled: async (data:{}) => {
+		
+	}
+}
+
+export default UserService;
