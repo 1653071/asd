bbb56376126652c56d2fdcee3685640a96150504
Nguyen Minh Quang <1653071@student.hcmus.edu.vn>
2022-10-12 07:56:34 +0700

    first commit

--- middlewares/validate.middleware.ts
+++ middlewares/validate.middleware.ts
@@ -2,21 +2,21 @@ const { check } = require('express-validator');
 
 let validateRegisterUser = () => {
 	return [
-		check('user.username', 'username does not Empty').not().isEmpty(),
-		check('user.username', 'username must be Alphanumeric').isAlphanumeric(),
-		check('user.username', 'username more than 6 degits').isLength({ min: 6 }),
-		check('user.email', 'Invalid does not Empty').not().isEmpty(),
-		check('user.email', 'Invalid email').isEmail(),
-		check('user.birthday', 'Invalid birthday').isISO8601('yyyy-mm-dd'),
-		check('user.password', 'password more than 6 degits').isLength({ min: 6 })
+		check('title', 'username does not Empty').not().isEmpty(),
+		check('title', 'username must be Alphanumeric').isAlphanumeric(),
+		check('playgroundId', 'playgroundid not does not Empty').not().isEmpty(),
+		check('playgroundId', 'playground id must be number').isNumeric(),
+		check('team', 'teamdoes not Empty').not().isEmpty(),
+		check('team', 'Invalid must be number').isNumeric(),
+		
 	];
 }
 
 let validate = {
 	validateRegisterUser: [
-		check('user.username', 'username does not Empty').not().isEmpty(),
-		check('user.username', 'username must be Alphanumeric').isAlphanumeric(),
-		check('user.username', 'username more than 6 degits').isLength({ min: 6 }),
+		check('message', 'message does not Empty').not().isEmpty(),
+		check("message", 'message must be Alphanumeric').isAlphanumeric(),
+		check('fileAttach', 'file attach much be array').isArray(),
 		check('user.email', 'Invalid does not Empty').not().isEmpty(),
 		check('user.email', 'Invalid email').isEmail(),
 		check('user.birthday', 'Invalid birthday').isISO8601('yyyy-mm-dd'),
